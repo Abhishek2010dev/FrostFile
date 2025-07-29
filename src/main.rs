0d7e3b1bf12f229c9a56bcb6f9226c220735a3ed
Abhishek2010dev <abhishekkumar2010dev@gmail.com>
2025-07-29 16:10:45 +0530

    features: Egui setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+mod ui;
+
+use eframe::egui;
+
+use crate::ui::FrostFile;
+
+fn main() -> eframe::Result {
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([800.0, 600.0]),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "Frost File",
+        options,
+        Box::new(|_| Ok(Box::<FrostFile>::default())),
+    )
 }
