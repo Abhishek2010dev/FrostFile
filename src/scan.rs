eb97d63ca4a1b6bc023277347c3a609d6a17e439
Abhishek2010dev <abhishekkumar2010dev@gmail.com>
2025-07-29 21:20:14 +0530

    features: Scan File function

--- src/scan.rs
+++ src/scan.rs
@@ -1,4 +1,6 @@
-use std::{collections::HashSet, sync::LazyLock};
+use std::{collections::HashSet, fs::File, io::Read, path::Path, sync::LazyLock};
+
+use sha2::{Digest, Sha256};
 
 static MALWARE_HASHES: LazyLock<HashSet<String>> = LazyLock::new(|| {
     include_str!("../full-hash-sha256-aa.txt")
@@ -8,8 +10,32 @@ static MALWARE_HASHES: LazyLock<HashSet<String>> = LazyLock::new(|| {
 });
 
 #[derive(Debug)]
-pub enum ScanResult {
+pub enum ScanResult<'a> {
     Clean,
-    Infected(String), // virus or signature name
-    Error(String),    // IO or read error
+    Infected { file_path: &'a Path },
+    Error(String),
+}
+
+pub fn scan_file(path: &Path) -> ScanResult {
+    let mut file = match File::open(path) {
+        Ok(t) => t,
+        Err(err) => return ScanResult::Error(format!("Failed to open file: {err}")),
+    };
+
+    let mut hasher = Sha256::new();
+    let mut buffer = [0u8; 8192]; // 8KB
+
+    loop {
+        match file.read(&mut buffer) {
+            Ok(0) => break,
+            Ok(n) => hasher.update(&buffer[..n]),
+            Err(err) => return ScanResult::Error(format!("Failed to read file: {err}")),
+        }
+    }
+
+    let hash_hex = format!("{:x}", hasher.finalize());
+    if MALWARE_HASHES.contains(&hash_hex) {
+        return ScanResult::Infected { file_path: path };
+    }
+    ScanResult::Clean
 }
